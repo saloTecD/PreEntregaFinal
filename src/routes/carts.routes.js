ddba39e7babca79068fbf50ffc34692c3f2fcb30
sergio lopez <salo.tec.d@gmail.com>
2023-04-19 11:51:37 -0500

    validacion de producto existente al agregar al carrito finalizada

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const router = express.Router()
 const cartManager = require("../cartManager")
+const productManager = require("../productManager")
 
 router.post(`/carts`,async(req,res)=>{
     res.status(200).send(await cartManager.cart.createCart())
@@ -15,7 +16,12 @@ router.get(`/carts/:cid`,async(req,res)=>{
 router.post(`/carts/:cid/product/:pid`,async(req,res)=>{
     let cid=parseInt(req.params.cid)
     let pid=parseInt(req.params.pid)
+    if(await productManager.store.getProductById(pid)==="Not Found"){
+        
+        res.status(200).send("El producto que quieres agregar no existe")
+    }else{
     res.status(200).send(await cartManager.cart.addCartProduct(cid,pid))
+    }
 })
 
 module.exports = router
